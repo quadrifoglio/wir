9b50f5115a207b1a6b6caed09382f16ff3f07fab
quadrifoglio <clement@dreamvids.fr>
2016-04-08 19:55:57 +0200

    Rework: switching to Go

--- README.md
+++
@@ -1,5 +0,0 @@
-# wir
-
-HTTP virtualization node control system.
-
-Support will be primarily for the QEMU/KVM hypervisor.
--- include/wird/utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "wird/wird.h"
-
-void         wird_log(const char* fmt, ...);
-const char*  errstr(int err);
-bool         isnum(const char* s);
